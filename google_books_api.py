60624357b3755287ff553617454977dd828e5dc2
nesosuke <nesosuke@gmail.com>
2020-03-20 15:53:41 +0900

    書名、著者の取得できるようになりました🎉

--- google_books_api.py
+++ google_books_api.py
@@ -1,13 +1,14 @@
 #Google BooksでAPIを叩く
 import requests as req
-import json 
+ 
 ISBN = 9784873117386
 response = req.get(
     'https://www.googleapis.com/books/v1/volumes',
     params={
         "q": "isbn=" + str(ISBN)
     })
-response = dict(response.json()).get("items")
-#response = json.loads(response)
-
-print(response)
+BookInfo = dict(response.json()).get("items")[0]["volumeInfo"]
+BookTitle = BookInfo["title"]
+BookAuthor = BookInfo["authors"][0]
+print("Title: " + BookTitle)
+print("Author: " + BookAuthor)
